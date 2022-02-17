bdeac940842722cc0bbd23e4619cc36b1b96000b
mofe <mofeomotosho8@gmail.com>
2022-02-17 15:34:12 -0300

    setting up react

--- party_mix/settings.py
+++ party_mix/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework',
+    'frontend.apps.FrontendConfig'
+    
     
 ]
 
